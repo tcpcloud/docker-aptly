680ba08ef4edb7034aab621976d401db205223c0
Filip Pytloun <filip@pytloun.cz>
2017-05-29 18:07:20 +0200

    Fix gpg key generation

--- docker/files/entrypoint.sh
+++ docker/files/entrypoint.sh
@@ -3,6 +3,7 @@
 USER_ID=${LOCAL_USER_ID:-501}
 FULL_NAME=${FULL_NAME:-"Aptly Repo Signing"}
 EMAIL_ADDRESS=${EMAIL_ADDRESS:-root@localhost}
+GPG_KEY_LENGTH=4096
 export HOME=/var/lib/aptly
 
 echo "Creating user aptly with UID $USER_ID"
@@ -16,18 +17,15 @@ fi
 if [ ! -e ${HOME}/.gnupg/secret.gpg ]; then
     echo "Generating new GPG keypair.."
     [ -e ${HOME}/gpg_batch ] || cat << EOF > ${HOME}/gpg_batch
-%echo Generating a GPG key, might take a while
-Key-Type: RSA
-Key-Length: 4096
-Subkey-Type: ELG-E
-Subkey-Length: 1024
+%echo Generating a default key
+Key-Type: default
+Key-Length: ${GPG_KEY_LENGTH}
+Subkey-Type: default
 Name-Real: ${FULL_NAME}
-Name-Comment: Aptly Repo Signing
+Name-Comment: Aptly repository signing key
 Name-Email: ${EMAIL_ADDRESS}
 Expire-Date: 0
 %no-protection
-%pubring ${HOME}/.gnupg/pubring.gpg
-%secring ${HOME}/.gnupg/secring.gpg
 %commit
 %echo done
 EOF
