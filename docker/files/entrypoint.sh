41b49f07801127d0d574ca66d64a59bc1b520585
Filip Pytloun <filip@pytloun.cz>
2017-10-06 14:14:26 +0200

    Enhance logging

--- docker/files/entrypoint.sh
+++ docker/files/entrypoint.sh
@@ -6,19 +6,42 @@ FULL_NAME=${FULL_NAME:-"Aptly Repo Signing"}
 EMAIL_ADDRESS=${EMAIL_ADDRESS:-root@localhost}
 GPG_KEY_LENGTH=${GPG_KEY_LENGTH:-4096}
 GPG_BINARY=${GPG_BINARY:-gpg1}
+# Syslog numeric log level, see https://tools.ietf.org/html/rfc5424
+# Defaults to warning
+LOG_LEVEL=${LOG_LEVEL:-4}
 export HOME=/var/lib/aptly
 
-echo "Creating user aptly with UID $USER_ID" 1>&2
+[ $LOG_LEVEL -gt 6 ] && set -x
+
+log_err() {
+    if [[ $LOG_LEVEL -eq 0 ]]; then
+        echo "[ERROR] $*" 1>&2
+    fi
+}
+
+log_warn() {
+    if [[ $LOG_LEVEL -gt 3 ]]; then
+        echo "[WARN] $*" 1>&2
+    fi
+}
+
+log_info() {
+    if [[ $LOG_LEVEL -gt 5 ]]; then
+        echo "[INFO] $*" 1>&2
+    fi
+}
+
+log_info "Creating user aptly with UID $USER_ID"
 getent group aptly >/dev/null || groupadd --system -g $GROUP_ID aptly
-getent passwd aptly >/dev/null || useradd --system --shell /bin/bash -u $USER_ID -g aptly -d ${HOME} -m aptly
+getent passwd aptly >/dev/null || useradd --system --shell /bin/bash -u $USER_ID -g aptly -d ${HOME} -m aptly 1>/dev/null 2>/dev/null
 
 if [ $(stat -c '%u' ${HOME}) != $USER_ID ]; then
-    echo "Fixing ${HOME} permissions.." 1>&2
+    log_warn "Fixing ${HOME} permissions.."
     chown -R aptly:aptly ${HOME}
 fi
 
 if [ ! -e ${HOME}/.gnupg ]; then
-    echo "Generating new GPG keypair.." 1>&2
+    log_warn "Generating new GPG keypair.."
     [ -e ${HOME}/gpg_batch ] || cat << EOF > ${HOME}/gpg_batch
 %echo Generating a default key
 Key-Type: RSA
@@ -35,10 +58,10 @@ Expire-Date: 0
 EOF
     gosu aptly bash -c "$GPG_BINARY --batch --gen-key ${HOME}/gpg_batch"
 
-    echo "Importing distribution keyring.." 1>&2
+    log_info "Importing distribution keyring.."
     gosu aptly bash -c "for i in /usr/share/keyrings/*; do $GPG_BINARY --no-default-keyring --keyring \$i --export | $GPG_BINARY --import; done"
 
-    echo "Storing public key in ${HOME}/public/public.gpg" 1>&2
+    log_info "Storing public key in ${HOME}/public/public.gpg"
     [ -d ${HOME}/public ] || gosu aptly bash -c "mkdir ${HOME}/public"
     if [ $(stat -c '%u' ${HOME}/public) != $USER_ID ]; then
         echo "Fixing ${HOME}/public permissions.." 1>&2
